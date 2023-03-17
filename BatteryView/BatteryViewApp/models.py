1e0a81278484424ad826c6877fd80a62d6fdc64a
Casper van Dam <cvdam01@gmail.com>
2023-03-17 22:08:12 +0100

    new requirements

--- BatteryView/BatteryViewApp/models.py
+++ BatteryView/BatteryViewApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+#todo: 
+#columns: datum, array: [prijzen], aantal pieken, piektijden (inkooptijden, verkooptijd) [1: {inkoop:, verkoop}], totale opbrengst, totale gehandelde energie (kWh). 
+def day(models):
+    pass
